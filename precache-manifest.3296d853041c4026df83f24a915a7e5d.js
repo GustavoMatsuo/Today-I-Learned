6a12ab88e0ddb9dc23fa2a2f7bcca90ee1ee2e74
Gustavo Matsuo <GustavoMatsuoF@gmail.com>
2020-04-06 18:48:40 -0300

    Updates

---
+++ precache-manifest.3296d853041c4026df83f24a915a7e5d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e499255113980ebbc666c9ebd43ba39",
+    "url": "/Today-I-Learned/index.html"
+  },
+  {
+    "revision": "70811eeab116aca13fa6",
+    "url": "/Today-I-Learned/static/css/main.e1ff0124.chunk.css"
+  },
+  {
+    "revision": "ef692b4d210b95953d50",
+    "url": "/Today-I-Learned/static/js/2.f767dec8.chunk.js"
+  },
+  {
+    "revision": "7c8b7808c34cb59049374f5d75e06b5f",
+    "url": "/Today-I-Learned/static/js/2.f767dec8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70811eeab116aca13fa6",
+    "url": "/Today-I-Learned/static/js/main.48f0d660.chunk.js"
+  },
+  {
+    "revision": "ad923084f2ce4d4d6574",
+    "url": "/Today-I-Learned/static/js/runtime-main.28c49224.js"
+  },
+  {
+    "revision": "c850bb8dc4db784a159bb3e6b80bacda",
+    "url": "/Today-I-Learned/static/media/2020-04-06.c850bb8d.md"
+  },
+  {
+    "revision": "74f58ac792722fced3bf61483e165708",
+    "url": "/Today-I-Learned/static/media/github-white.74f58ac7.svg"
+  },
+  {
+    "revision": "617870e422d4b75087cdcf9ff298032e",
+    "url": "/Today-I-Learned/static/media/github.617870e4.svg"
+  },
+  {
+    "revision": "2d0c2a366389ce2b69aec277480e28c4",
+    "url": "/Today-I-Learned/static/media/linkedin-white.2d0c2a36.svg"
+  },
+  {
+    "revision": "eb091578f0560c7b58b5bf82eb48f485",
+    "url": "/Today-I-Learned/static/media/linkedin.eb091578.svg"
+  }
+]);
